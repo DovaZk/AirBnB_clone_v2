386dffb2007d0a8b74e96eae58ea8ea38cbdc64a
DovaZk <silva.joel.08@gmail.com>
2021-08-26 21:31:24 -0500

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
+""" Pack web static """
+from fabric.api import *
+from os import path
 
-'''
-Configure the server
-'''
-
-from fabric.api import put, run, env
-from os.path import exists
-
-
-env.hosts = ['34.138.89.43', '54.90.156.100']
+env.hosts = ["34.139.1.80", "34.75.176.177"]
 
 
 def do_deploy(archive_path):
-    '''distributes an archive to the web servers'''
-    if exists(archive_path) is False:
+    """distributes an archive to your web servers"""
+    if not path.exists(archive_path):
         return False
     try:
-        file_n = archive_path.split('/')[-1]
-        no_ext = file_n.split('.')[0]
-        path = '/data/web_static/releases/'
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        dire = "data/web_static/releases"
+        file_name = archive_path.split('/')[-1]
+        wext = file_name.split('.')[0]
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p mkdir -p /{}/{}".format(dire, wext))
+        run("tar -xzf /tmp/{} -C /{}/{}/".format(file_name, dire, wext))
+        run("rm /tmp/{}".format(file_name))
+        run("mv /{}/{}/web_static/* /{}/{}/".format(dire, wext, dire, wext))
+        run("rm -rf /{}/{}/web_static".format(dire, wext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /{}/{}/ /data/web_static/current".format(dire, wext))
         return True
-    except:
+
+    except Exception:
         return False
